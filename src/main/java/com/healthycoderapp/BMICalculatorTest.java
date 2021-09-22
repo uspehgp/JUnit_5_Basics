b82b516dea1b949ee0930fa8d14273199d762144
uspehgp <uspehgp@gmail.com>
2021-09-22 15:53:59 +0300

    create should_ReturnBMIScoresArray_whenCoderListNotEmpty test

--- src/main/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/main/java/com/healthycoderapp/BMICalculatorTest.java
@@ -73,6 +73,7 @@ class BMICalculatorTest {
                 () -> assertEquals(98.0, coderWorstBmi.getWeight())
         );
     }
+
     @Test
     void shouldReturnCoderWithWorstBMI_whenCoderListEmpty() {
         //given
@@ -83,5 +84,21 @@ class BMICalculatorTest {
 
         //then
         assertNull(coderWorstBmi);
+    }
+
+    @Test
+    void should_ReturnBMIScoresArray_whenCoderListNotEmpty() {
+        //given
+        List<Coder> coders = new ArrayList<>();
+        coders.add(new Coder(1.80, 60.0));
+        coders.add(new Coder(1.82, 98.0));
+        coders.add(new Coder(1.82, 64.7));
+        double expected[] = {18.52, 29.59, 19.53};
+
+        //when
+        double[] bmiScores = BMICalculator.getBMIScores(coders);
+
+        //then
+        assertArrayEquals(expected, bmiScores);
     }
 }
